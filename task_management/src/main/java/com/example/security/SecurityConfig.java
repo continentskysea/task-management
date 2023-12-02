0af887a578ad6c49cb89a2254d4ad925cc3fcce6
yoshii <yoshiikempo@gmail.com>
2023-12-03 00:51:05 +0900

    ハッシュアルゴリズム定義に関する処理のコメント修正とシステム全体の設定に関するセキュリティ設定のカスタマイズ処理を追加

--- task_management/src/main/java/com/example/security/SecurityConfig.java
+++ task_management/src/main/java/com/example/security/SecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -35,4 +38,30 @@ public class SecurityConfig {
 		
 		return http.build();
 	}
+	
+	/**
+	 * ハッシュアルゴリズムの定義
+	 * 
+	 * @return 生成されたパスワードのハッシュ値
+	 */
+	@Bean
+	protected PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+
+	
+	/**
+	 * Webシステム全体のセキュリティ設定のカスタマイズ
+	 * 
+	 * @return
+	 */
+	@Bean
+	protected WebSecurityCustomizer webSecurityCustomizer() {
+		return web -> web.ignoring().requestMatchers("/css/**").requestMatchers("/js/**");
+	}
+	
+	
+	
+	
 }
