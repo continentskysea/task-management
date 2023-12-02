c297b6ed513747fed39c5a1fe85ec7ebe1ea0a2c
yoshii <yoshiikempo@gmail.com>
2023-12-02 22:51:51 +0900

    設定に関するコメントの追記

---
+++ task_management/src/main/java/com/example/security/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.example.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * セキュリティ設定クラス
+ * @author yoshi
+ *
+ */
+// Configuration を明示
+@Configuration
+// Spring Security の設定を有効化する
+@EnableWebSecurity
+public class SecurityConfig {
+	@Bean
+	protected SecurityFilterChain securityFilterChainConfig(HttpSecurity http) throws Exception {
+		// ログイン情報
+		http.formLogin(login -> login
+				.loginProcessingUrl("/login")// ログイン処理の
+				.loginPage("/loginForm") // ログインページの指定
+				.usernameParameter("email") // ログインページのメールアドレス
+				.passwordParameter("password") // ログインページのパスワード
+				.defaultSuccessUrl("/home", true) // ログイン成功時のパス
+				.failureUrl("/loginForm?error") // ログイン失敗時のパス
+				.permitAll()
+		// ログアウト処理		
+		).logout(logout -> logout
+				.logoutUrl("/logout") // ログアウト処理のパス
+				.logoutSuccessUrl("loginForm") // ログアウト成功時のパス
+		);
+		
+		return http.build();
+	}
+}
