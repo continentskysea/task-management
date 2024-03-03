3470c9cf3e9ef0f31d7656f6171fcced979fdf4f
yoshii <yoshiikempo@gmail.com>
2024-03-04 00:07:09 +0900

    タイマースタート処理の配置クラスを修正

--- task_management/src/main/java/com/example/controller/TimerController.java
+++ task_management/src/main/java/com/example/controller/TimerController.java
@@ -8,10 +8,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.entity.Task;
+import com.example.entity.TimerLog;
 import com.example.entity.TimersSetting;
 import com.example.service.TaskService;
+import com.example.service.TimerLogService;
 import com.example.service.TimersSettingService;
 
 /**
@@ -25,12 +29,19 @@ public class TimerController {
 	// フィールドの追加
 	private final TimersSettingService timersSettingService;
 	private final TaskService taskService;
+	// タイマーログサービスのフィールドを追加
+	private final TimerLogService timerLogService;
 	
 	// コンストラクタインジェクション
 	@Autowired
-	public TimerController(TimersSettingService timersSettingService, TaskService taskService) {
+	public TimerController(
+			TimersSettingService timersSettingService,
+			TaskService taskService
+			,TimerLogService timerLogService
+			) {
 		this.timersSettingService = timersSettingService;
 		this.taskService = taskService;
+		this.timerLogService = timerLogService;
 	}
 	
 	/**
@@ -127,6 +138,49 @@ public class TimerController {
 			model.addAttribute("timersSetting", timersSetting);
 			return "timers/breakTimer";
 		}
+	}
+	
+	
+	/**
+	 * タイマー起動
+	 * 
+	 * @return 集中タイマー画面
+	 */
+	@PostMapping("/startTimer")
+	public String startTimer(
+			@RequestParam("focusTime") Integer focusTime, 
+			@RequestParam("timerType") String timerType,  
+			@RequestParam("settingId") Integer settingId,
+			RedirectAttributes ra,
+			Model model) {
+		// ログ登録用にオブジェクトを生成する
+		TimerLog timerLog = new TimerLog();
+		
+		// 開始時間を設定する
+		timerLog.setStartTime(focusTime);
+		// 集中タイマーか休憩タイマーを判定する
+		if (timerType == "1") {
+			timerLog.setTimerType("集中");
+		} else if (timerType == "2") {
+			timerLog.setTimerType("休憩");
+		}
+
+		// TIMERS_SETTINGSテーブルから対応する設定IDを取得
+		TimersSetting timersSetting = timersSettingService.findById(settingId);
+		
+		// タイマー設定オブジェクトのnullチェック
+		if (timersSetting == null) {
+			ra.addFlashAttribute("error_message", "タイマー設定IDに対する設定が見つかりません");
+			return "redirect:/focusTimer";
+		}
+		
+		// タイマー設定IDを設定
+		timerLog.setSettingId(timersSetting.getId());
+		
+		// ログに登録する
+		timerLogService.save(timerLog);
+		
+		return "redirect:/focusTimer";
 	}
 }
 
