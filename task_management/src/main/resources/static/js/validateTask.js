5155f05e65cbf2aff680ca0ea03b077fec1791d0
yoshii <yoshiikempo@gmail.com>
2024-04-07 16:29:32 +0900

    バリデーションの修正

--- task_management/src/main/resources/static/js/validateTask.js
+++ task_management/src/main/resources/static/js/validateTask.js
@@ -7,13 +7,11 @@ $(document).ready(function() {
         let taskName = $("#taskName").val();
         let priority = $("#priority").val();
         let dueDate = $("#dueDate").val();
-        
         // バリデーションフラグ
         let isValid = true;
 
-
         // タスク名が入力されているかどうかをチェック
-        if (taskName === "" || taskName === null) {
+        if (taskName == "" || taskName === null) {
             // タスクが入力されていない場合はエラーを出してフォーム送信を中止する
             alert("タスクを入力してください");
             isValid = false;
@@ -25,12 +23,8 @@ $(document).ready(function() {
             // 優先順位が入力されていない場合はエラーを出してフォーム送信を中止する
             alert("優先順位を入力してください");
             isValid = false;
-            return isValid;	
-        } else if (priority <= 0 || priority > 30) {
-            // 優先順位が0以下または30を超える場合はエラーを出してフォーム送信を中止する
-            alert("優先順位は1から30までの数字を入力してください");
-            isValid = false;
-            return isValid;	
+            return isValid;
+            
         }
         
         /// 期日が入力されているかどうかをチェック
@@ -39,6 +33,17 @@ $(document).ready(function() {
             alert("期日を入力してください");
             isValid = false;
             return isValid;	
-        }        
+        }
+
+        // 優先順位が1から30までの間で入力されているかチェック
+        if (priority <= 0 || priority > 30) {
+            // 優先順位が0以下または30を超える場合はエラーを出してフォーム送信を中止する
+            alert("優先順位は1から30までの数字を入力してください");
+            isValid = false;
+            return isValid;	
+        }
+
+        // フォームエラー送信を中止する
+        return isValid;	
     });
 });
