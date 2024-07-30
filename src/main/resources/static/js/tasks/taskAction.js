d65908090f0fc4a1ab87ca2657020543dab49bb5
Yoshii-Kaito-1105 <it-tennoji130@dc-itex.com>
2024-07-30 11:59:47 +0900

    タスク登録画面のUI修正#1

--- src/main/resources/static/js/tasks/taskAction.js
+++ src/main/resources/static/js/tasks/taskAction.js
@@ -10,6 +10,32 @@ $(document).ready(function() {
 		window.location.href = '/getCreateTask'; 
 	});
 	/**
+	 * 登録データを送信する
+	 */
+	$(".registarTask").on("click", function() {
+
+		// htmlに入力された値を格納する
+		let taskNameInput = $("#taskNameInput").val();
+		let priorityInput = $("#priorityInput").val();
+		let dueDateInput = $("#dueDateInput").val();
+
+		// 入力データをオブジェクトに格納
+		let task = {
+			taksName: taskNameInput,
+			priority: priorityInput, 
+			dueDate: dueDateInput
+		};
+
+		// フォームの値をセットする
+		$(".taskFormContainer input[ name = 'taskName' ]").val(task.taksName);
+		$(".taskFormContainer input[ name = 'priority' ]").val(task.priority);
+		$(".taskFormContainer input[ name = 'dueDate' ]").val(task.dueDate);
+		
+		// フォームを送信
+		$(".taskRegistarForm").submit();
+	});
+
+	/**
 		タスク編集画面へ遷移する
 	 */
 	$(".taskEditRelation").on("click", function() {
@@ -45,8 +71,9 @@ $(document).ready(function() {
 	});
 	
 	// タスク管理初期画面へ遷移
-	$(".taskHomeRelation").on("click", function() {
+	$(".getTaskHome").on("click", function() {
 		window.location.href = '/getTask';
+		return false;
 	});
 	
 	/**
--- src/main/resources/static/js/tasks/validateTask.js
+++ src/main/resources/static/js/tasks/validateTask.js
@@ -2,16 +2,16 @@
  * タスク登録のバリデーション
  */
 $(document).ready(function() {
-    $("button").on("click", function() {
+    $("registarTask").on("click", function() {
         // フォームから入力値を取得
-        let taskName = $("#taskName").val();
-        let priority = $("#priority").val();
-        let dueDate = $("#dueDate").val();
+        let taskNameInput = $("#taskNameInput").val();
+        let priorityInput = $("#priorityInput").val();
+        let dueDateInput = $("#dueDateInput").val();
         // バリデーションフラグ
         let isValid = true;
 
         // タスク名が入力されているかどうかをチェック
-        if (taskName == "" || taskName === null) {
+        if (taskNameInput == "" || taskNameInput === null) {
             // タスクが入力されていない場合はエラーを出してフォーム送信を中止する
             alert("タスクを入力してください");
             isValid = false;
@@ -19,7 +19,7 @@ $(document).ready(function() {
         }
         
         // 優先順位が入力されているかどうかをチェック
-        if (priority === "" || priority === null) {
+        if (priorityInput === "" || priorityInput === null) {
             // 優先順位が入力されていない場合はエラーを出してフォーム送信を中止する
             alert("優先順位を入力してください");
             isValid = false;
@@ -28,7 +28,7 @@ $(document).ready(function() {
         }
         
         /// 期日が入力されているかどうかをチェック
-        if (dueDate === "" || dueDate === null) {
+        if (dueDateInput === "" || dueDateInput === null) {
             // 期日が入力されていない場合はエラーを出してフォーム送信を中止する
             alert("期日を入力してください");
             isValid = false;
@@ -36,7 +36,7 @@ $(document).ready(function() {
         }
 
         // 優先順位が1から30までの間で入力されているかチェック
-        if (priority <= 0 || priority > 30) {
+        if (priorityInput <= 0 || priorityInput > 30) {
             // 優先順位が0以下または30を超える場合はエラーを出してフォーム送信を中止する
             alert("優先順位は1から30までの数字を入力してください");
             isValid = false;
