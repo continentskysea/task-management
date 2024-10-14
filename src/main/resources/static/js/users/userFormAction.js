605e31ce3574bd93be4898c5390fd5e9bcb88318
yoshii <yoshiikempo@gmail.com>
2024-10-15 02:32:29 +0900

    ユーザー編集フォームの改修

--- src/main/resources/static/js/users/userFormAction.js
+++ src/main/resources/static/js/users/userFormAction.js
@@ -3,8 +3,14 @@
  */
 $(document).ready(function () {
     // ユーザー管理初期画面へ遷移
-    $(".getUserHome").on("click", function () {
+    $(".getUserHome").on("click", function() {
         window.location.href = "/getUserHome";
         return false;
     });
+
+    // ユーザー一覧画面へ遷移
+    $(".getListUsers").on("click", function() {
+        window.location.href = "/getListUsers";
+        return false;
+    })
 });
