21ff25c2d176c809b9c215614465978141d915fb
yoshii <yoshiikempo@gmail.com>
2025-01-16 22:45:49 +0900

    パスワードクラスの実装#1

---
+++ src/main/java/com/example/domain/Password.java
@@ -0,0 +1,27 @@
+package com.example.domain;
+
+public class Password {
+    private String password;
+
+    public Password(String password) {
+        this.password = password;
+    }
+
+
+    public boolean validation(String password) {
+
+    }
+
+    /**
+     * パスワードの未入力チェック
+     * @param password
+     * @return true/false
+     */
+    public boolean isPasswordInput(String password) {
+        return password.equals(password);
+    }
+
+    public boolean numberInputIsVertify(String password) {
+
+    }
+}
