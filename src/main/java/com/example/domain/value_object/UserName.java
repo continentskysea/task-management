8ab187feecddfc86ebd38444e8ce50dbde4686fb
yoshii <yoshiikempo@gmail.com>
2025-01-23 23:25:49 +0900

    ユーザー名値オブジェクト実装#2

--- src/main/java/com/example/domain/value_object/UserName.java
+++ src/main/java/com/example/domain/value_object/UserName.java
@@ -13,42 +13,65 @@ public class UserName {
     /**
      * ユーザー名入力チェック
      * @param username
-     * @return true/false
+     * @return true = 入力されている / false = 未入力
      */
     private boolean isUsernameInput(String userName) {
         return userName.equals(userName);
     }
 
     /**
-     * ユーザー名が入力状況の結果を返す
+     * ユーザー名が入力・未入力の検証結果を返す
      * @param userName
      * @return 検証結果
      */
-    private String userNameInputVertificationResult(String userName) {
+    private String userNameVertificationResult(String userName) {
         return isUsernameInput(userName) ? "ユーザー名が未入力です。" : "問題ありません。";        
     }
 
     /**
      * ユーザー名文字数チェック
      * @param userName
-     * @return true/false
+     * @return true = 3文字以上12文字以下のユーザー名 / false = それ以外
      */
-    public boolean userNameLengthCheck(String userName) {
+    private boolean userNameLengthCheck(String userName) {
         return userName.length() >= 3 && userName.length() <= 12;
+    }
+
+    /**
+     * ユーザー名の文字数の検証結果を返す
+     * @param userName
+     * @return 検証結果
+     */
+    private String userNameInputLengthVertificationResult(String userName) {
+        return userNameLengthCheck(userName) ? "問題ありません。" : "ユーザー名は3文字以上12文字以下で入力してください。";
     }
 
     /**
      * ユーザー名が漢字かチェックする機能
      * @param userName
-     * @return true/false
+     * @return true = 漢字 / false = それ以外(アルファベット・ひらがな・カタカナ)
      */
     public boolean userNameRegex(String userName) {
         return userName.matches("[\\p{IsHan}}]*");
     }
 
-    public String getUserNameValue() {
-        userNameInputVertificationResult(userName);
+    /**
+     * ユーザー名が漢字で入力されいるかの検証結果を返す
+     * @param userName
+     * @return
+     */
+    private String userNameRegexVertificationResult(String userName) {
+        return userNameRegex(userName) ? "問題ありません。" : "ユーザー名は漢字で入力してください。";
+    }
 
+    /**
+     * ユーザー名を取得する
+     * @return ユーザー名
+     */
+    public String getUserNameValue() {
+        userNameVertificationResult(userName);
+        userNameInputLengthVertificationResult(userName);
+        userNameRegexVertificationResult(userName);
         return userName;
     }
 }
