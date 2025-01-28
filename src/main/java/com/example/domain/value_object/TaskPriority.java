c092ed3c7c3ebe49506c75b664250ba1f53c18cb
yoshii <yoshiikempo@gmail.com>
2025-01-28 23:35:10 +0900

    優先順位値オブジェクトの実装

--- src/main/java/com/example/domain/value_object/TaskPriority.java
+++ src/main/java/com/example/domain/value_object/TaskPriority.java
@@ -10,7 +10,26 @@ public class TaskPriority {
         this.taskPriority = taskPriority;
     }
 
-    public Long getTasKPriority() {
+    /**
+     * 優先順位の数値をチェックする
+     * @param taskPriority
+     * @return true = 1文字以上30文字以内 / false = それ以外
+     */
+    private boolean taskPriorityInput(Long taskPriority) {
+        return taskPriority >= 1 && taskPriority <= 30;
+    }
+
+    /**
+     * 優先順位の検証結果を返す
+     * @param taskPriority
+     * @return 検証結果
+     */
+    private String taskPriorityInputVertificationResult(Long taskPriority) {
+        return taskPriorityInput(taskPriority) ? "" : "優先順位は1から30までの数字を入力してください";
+    }
+
+    public Long getTasKPriority(Long taskPriority) {
+        taskPriorityInputVertificationResult(taskPriority);
         return taskPriority;
     }
 }
