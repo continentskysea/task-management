69e36dd29381eb0b88a4387d59e0471fbd0024c9
yoshii <yoshiikempo@gmail.com>
2025-05-13 05:35:15 +0900

    タスク集約エンティティの実装

--- src/main/java/com/example/domain/entity/Task.java
+++ src/main/java/com/example/domain/entity/Task.java
@@ -1,13 +1,11 @@
 package com.example.domain.entity;
 
-// import java.time.LocalDate;
-
-// import jakarta.persistence.Column;
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import com.example.domain.value_object.task.TaskId;
 import com.example.domain.value_object.task.TaskName;
@@ -17,23 +15,41 @@ import com.example.domain.value_object.task.status.Status;
 import com.example.domain.value_object.user.UserId;
 
 /**
- * タスク情報のデータクラス
+ * タスク情報集約エンティティ
  * @author yoshi
  *
  */
-
-
-// @Entity
-// @Table(name = "tasks", schema = "verification")
+@Entity
+@Table(name = "tasks", schema = "verification")
 public class Task {
+
+    /**
+     * メンバフィールド
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private final TaskId id;
+
+    @Column(name = "name")
     private final TaskName name;
+
+    @Column(name = "priority")
     private final Priority priority;
+
+	@Column(name = "due_date")
     private final DueDate dueDate;
+
+    @Column(name = "status")
     private final Status status;
+
+    @Column(name = "user_id")
     private final UserId userId;
 
 
+    /**
+     * コンストラクタ
+     */
     public Task(
         TaskId id,
         TaskName name,
@@ -58,77 +74,4 @@ public class Task {
         return new Task(id, name, priority, dueDate, status, userId);
     }
 
-// 	@Id
-// 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-// 	@Column(name = "id")
-// 	private Long id; // タスクID
-	
-// 	@Column(name = "name")
-// 	private String name; // タスク名
-	
-// 	@Column(name = "priority")
-// 	private Long priority; // 優先順位
-	
-// 	@Column(name = "due_date") // 期日
-// 	private LocalDate dueDate;
-	
-// 	@Column(name = "user_id")
-// 	private Long userId; // ユーザーID
-	
-// 	// アクセサメソッド
-	
-// 	// タスクIDを取得する
-// 	public Long getId() {
-// 		return this.id;
-// 	}
-	
-// 	// タスクIDをセットする
-// 	public void setId(Long id) {
-// 		this.id = id;
-// 	}
-	
-	
-// 	// タスク名を取得する
-// 	public String getName() {
-// 		return this.name;
-// 	}
-	
-// 	// タスク名をセットする
-// 	public void setName(String name) {
-// 		this.name = name;
-// 	}
-	
-	
-// 	// 優先順位を取得する
-// 	public Long getPriority() {
-// 		return this.priority;
-// 	}
-	
-// 	// 優先順位をセットする
-// 	public void setPriority(Long priority) {
-// 		this.priority = priority;
-// 	}
-	
-	
-// 	// 期日を取得する
-// 	public LocalDate getDueDate() {
-// 		return this.dueDate;
-// 	}
-	
-// 	// 期日をセットする
-// 	public void setDueDate(LocalDate dueDate) {
-// 		this.dueDate = dueDate;
-// 	}	
-	
-	
-// 	// ユーザーIDを取得する
-// 	public Long getUserId() {
-// 		return this.userId;
-// 	}
-	
-// 	// ユーザーIDをセットする
-// 	public void setUserId(Long userId) {
-// 		this.userId = userId;
-// 	}
-	
 }
