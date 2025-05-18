9099b122bd36a27fae9c6ef299ba65a88854631a
yoshii <yoshiikempo@gmail.com>
2025-05-18 19:59:49 +0900

    テストの作成

--- src/test/java/com/example/domain/value_object/user/name/FirstNameTest.java
+++ src/test/java/com/example/domain/value_object/user/name/FirstNameTest.java
@@ -1,16 +1,20 @@
 package com.example.domain.value_object.user.name;
 
+import java.nio.file.Path;
+
 // import java.nio.file.Path;
 
 // import org.junit.jupiter.api.AfterAll;
 // import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-// import org.junit.jupiter.api.io.Clean;
+import org.junit.jupiter.api.io.TempDir;
 
 
 @SpringBootTest
 public class FirstNameTest {
+
+
     // @TempDir static Path tempDir;
     
 
@@ -19,11 +23,13 @@ public class FirstNameTest {
     //     System.out.println("FirstNameTest テスト開始");
     // }
 
+    
     @Test
-    public void testAddFirstName() throws Error {
-
+    public void testAddFirstName(@TempDir Path tempDir) throws Error {
+        var file = tempDir.resolve("file.txt");
         FirstName firstName = new FirstName("");
         firstName.getFirstNameValue();
+        System.out.println(file);
     }
 
     // @AfterAll
